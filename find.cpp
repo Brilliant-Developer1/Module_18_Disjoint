d65a5c7f5b169c82a3599f829c200f313a10bcae
Brilliant-Developer1 <email2obail@gmail.com>
2023-09-06 22:12:43 +0600

    first commit

---
+++ find.cpp
@@ -0,0 +1,79 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+int parent[8] = {-1, -1, 1, 1, 6, 4, -1, -1};
+
+int find(int n)
+{
+
+    while (parent[n] != -1)
+    {
+        n = parent[n];
+    }
+    return n;
+}
+
+int main()
+{
+
+    cout << find(7);
+    return 0;
+}
