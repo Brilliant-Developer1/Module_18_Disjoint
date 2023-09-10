7c8a657b3fa851babf4d0296454978f8b4824692
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-09-10 21:00:12 +0600

    union added

---
+++ union.cpp
@@ -0,0 +1,87 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+int parent[1000];
+
+void set(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        /* code */
+    }
+}
+
+int find(int n)
+{
+
+    while (parent[n] != -1)
+    {
+        n = parent[n];
+    }
+    return n;
+}
+
+int main()
+{
+
+    cout << find(7);
+    return 0;
+}
